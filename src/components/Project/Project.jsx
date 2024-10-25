f4eacded757a65d861a4c6fff5bfbe4307fd9d1b
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-10-25 15:09:12 +0600

    Project Update

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -85,6 +85,71 @@ const Project = () => {
                     <div className="main-project">
                         <div className='rounded-2xl flex flex-col justify-center items-center relative group'>
                             <div className='relative h-[350px] w-full rounded-2xl overflow-hidden'>
+                                <Image className='h-[350px] w-full rounded-2xl group-hover:scale-110 duration-300' width={500} height={300} src="/project/project3.png" alt="" />
+                                <div class="absolute inset-0 bg-gradient-to-b from-transparent via-black/50 to-black/80"></div>
+
+                            </div>
+                            <div className='absolute bottom-9 bg-opacity-60  projectName opacity-1 group-hover:opacity-0 duration-300'>
+                                <h1 className='text-white text-2xl'>QuickShop (Ecommerce Project)</h1>
+                                <p className='text-white'>Fast, secure, hassle-free shopping.</p>
+                            </div>
+                            <div className='absolute top-5 bg-opacity-60  projectName opacity-0 group-hover:opacity-100 duration-300'>
+                                <div className='flex items-center text-3xl gap-5'>
+                                    <Link target='_blank' href={'https://quickshop-4fa4e.web.app'}>
+                                        <div className='flex flex-col justify-center items-center'>
+                                            <div className='border w-12 p-2 bg-white rounded-full cursor-pointer'>
+                                                <FaLink />
+                                            </div>
+                                            <p className='text-[18px] text-white'>live link</p>
+                                        </div>
+                                    </Link>
+                                    <Link target='_blank' href={"https://github.com/kalidashodekare14/QuickShop-Client"}>
+                                        <div className='flex flex-col justify-center items-center'>
+                                            <div className='border w-12 p-2 bg-white rounded-full cursor-pointer'>
+                                                <FaGithub />
+                                            </div>
+                                            <p className='text-[18px] text-white'>client</p>
+                                        </div>
+                                    </Link>
+                                    <Link target='_blank' href={"https://github.com/kalidashodekare14/QuickShop-Server"}>
+                                        <div>
+                                            <div className='border w-12 p-2 bg-white rounded-full cursor-pointer'>
+                                                <BsGithub />
+                                            </div>
+                                            <p className='text-[18px] text-white'>server</p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className='absolute bottom-5 left-5 bg-opacity-60  opacity-0 group-hover:opacity-100  duration-300 group-hover:duration-300 translate-y-5 group-hover:translate-y-0'>
+                                <div className='flex flex-wrap gap-2'>
+                                    <div className='text-white bg-[#7e7c7c91] px-1 py-1 rounded-3xl'>
+                                        <h1>react.js</h1>
+                                    </div>
+                                    <div className='text-white bg-[#7e7c7c91] px-1 py-1 rounded-3xl'>
+                                        <h1>node.js</h1>
+                                    </div>
+                                    <div className='text-white bg-[#7e7c7c91] px-1 py-1 rounded-3xl'>
+                                        <h1>express.js</h1>
+                                    </div>
+                                    <div className='text-white bg-[#7e7c7c91] px-1 py-1 rounded-3xl'>
+                                        <h1>mongodb</h1>
+                                    </div>
+                                    <div className='text-white bg-[#7e7c7c91] px-1 py-1 rounded-3xl'>
+                                        <h1>react.js</h1>
+                                    </div>
+                                    <div className='text-white bg-[#7e7c7c91] px-1 py-1 rounded-3xl'>
+                                        <h1>tailwind css</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="main-project">
+                        <div className='rounded-2xl flex flex-col justify-center items-center relative group'>
+                            <div className='relative h-[350px] w-full rounded-2xl overflow-hidden'>
                                 <Image className='h-[350px] w-full rounded-2xl group-hover:scale-110 duration-300' width={500} height={300} src="/project/project2.png" alt="" />
                                 <div class="absolute inset-0 bg-gradient-to-b from-transparent via-black/50 to-black/80"></div>
 
